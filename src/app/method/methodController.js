144972dca7c49b5b594c92c7e0b2b4eb7fa5a66a
Hyeonseong <76834414+KiKi-Daehaksaeng@users.noreply.github.com>
2023-05-25 23:43:00 +0900

    UPDATE : Modifying API method

--- src/app/method/methodController.js
+++ src/app/method/methodController.js
@@ -1,57 +1,46 @@
 import { SUCCESS } from "../../../config/baseResponseStatus.js";
+import { insertMethod_Service, deleteMethod_Service, updateMethod_Service, getMethod_Service } from "./methodService.js";
 import dotenv from "dotenv";
-import request from "request";
+import request, { del } from "request";
 dotenv.config();
 
-export class medicineController {
-    addMedicine_Controller = async function (req, res) {
+export class methodController {
+    addMethod_Controller = async function (req, res) {
+        const id = req.body.id;
         const itemSeq = req.body.itemSeq;
-        const itemName = req.body.itemName;
-        const className = req.body.className;
-        const chart = req.body.chart;
-        const durSeq = req.body.durSeq;
-        const effect = req.body.effect;
-        const image = req.body.image;
+        const breakfast = req.body.breakfast;
+        const lunch = req.body.lunch;
+        const dinner = req.body.dinner;
+        const baw = req.body.baw;
+        const intakePeriod = req.body.intakePeriod;
+        const expPeriod = req.body.expPeriod;
 
-        const addResponse = await addMedicine_Service(itemSeq, itemName, className, durSeq, chart, effect, image);
+        const addResponse = await insertMethod_Service(id, itemSeq, breakfast, lunch, dinner, baw, intakePeriod, expPeriod);
         return res.send(addResponse);
     };
 
-    getMedicineByClass_Controller = async function (req, res) {
-        const className = req.body.className;
-        const response = await getMedicineByClass_Service(className);
-        return res.send(response);
-    };
-
-    getMedicineByItemSeq_Controller = async function (req, res) {
+    getMethod_Controller = async function (req, res) {
+        const id = req.body.id;
         const itemSeq = req.body.itemSeq;
-        const response = await getMedicineByItemSeq_Service(itemSeq);
+
+        const response = await getMethod_Service(id, itemSeq);
         return res.send(response);
     };
 
-    findSideEffect = async function (req, res) {
-        const itemSeq = req.query.keyword;
-        console.log(itemSeq);
+    deleteMethod_Controller = async function (req, res) {
+        const className = req.body.className;
 
-        var url = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList";
-        var queryParams = "?" + encodeURIComponent("serviceKey") + "=" + process.env.API_KEY; /* Service Key*/
-        queryParams += "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /* */
-        queryParams += "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("3"); /* */
-        queryParams += "&" + encodeURIComponent("itemSeq") + "=" + encodeURIComponent(itemSeq); /* */
-        queryParams += "&" + encodeURIComponent("type") + "=" + encodeURIComponent("json"); /* */
-        console.log("hello");
-        request(
-            {
-                url: url + queryParams,
-                method: "GET",
-            },
-            function (error, response, body) {
-                console.log("Status", response.statusCode);
-                console.log("Headers", JSON.stringify(response.headers));
-                console.log("Response received", body);
-                return res.send(body);
-            }
-        );
+        const response = await deleteMethod_Service(className);
+        return res.send(response);
+    };
+
+    updateMethod_Controller = async function (req, res) {
+        const id = req.body.id;
+        const itemSeq = req.body.itemSeq;
+        const expPeriod = req.body.expPeriod;
+
+        const response = await updateMethod_Service(id, itemSeq, expPeriod);
+        return res.send(response);
     };
 }
-export default new medicineController();
+export default new methodController();
